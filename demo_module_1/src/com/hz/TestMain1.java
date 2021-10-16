dc0c7017fce2b1b3ea01a00f6fd3b4be7530769e
Skarvvv <1403724251@qq.com>
2021-10-16 18:02:47 +0800

    尝试进行第三2次增改

--- demo_module_1/src/com/hz/TestMain1.java
+++ demo_module_1/src/com/hz/TestMain1.java
@@ -3,7 +3,7 @@ package com.hz;
 public class TestMain1 {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello gi");
     }
 
 }
